5c52ffd47be096153fda927484f4b2cf5821f0d1
Euwamahoro <e.uwamahoro@alustudent.com>
2023-07-28 01:28:02 -0700

    This is Enock

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,21 @@
-#!/usr/bin/python3
-
-"""This module contains the `BaseGeometry` class."""
-
+# 7-base_geometry.py
 
 class BaseGeometry:
-    """The base class of all `Geometry`"""
+    """The base class of all Geometry"""
 
     def area(self):
-        """Computes the `area` of a `Geometry`"""
+        """Computes the area of a Geometry"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """This validates a value.
 
         Args:
-            name(str): The `name` of the `value`
-            value(int): The `value` to be stored with the given `name`
+            name(str): The name of the value
+            value(int): The value to be stored
         """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
+
