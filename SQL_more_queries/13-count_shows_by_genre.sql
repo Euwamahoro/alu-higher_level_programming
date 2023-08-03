87c0a9cb4db2600772dd3378c55904c0f606e585
Euwamahoro <e.uwamahoro@alustudent.com>
2023-08-03 05:18:12 -0700

    This is Enock

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,8 @@
--- Importing database dump and listing all shows contained in the dump file
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-FROM tv_genres JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_genres.id = genre_id
+GROUP BY genre_id
+ORDER BY number_of_shows DESC;
