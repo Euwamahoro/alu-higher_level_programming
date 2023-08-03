1667dae42f3e34122af011f4fd5fbc0ba8bc8bb8
Euwamahoro <e.uwamahoro@alustudent.com>
2023-08-03 04:58:44 -0700

    This is Enock

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,3 @@
--- A script that creates the database hbtn_0d_usa and the table cities
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
-state_id INT NOT NULL FOREIGN KEY REFERENCES states(id), 
-name VARCHAR(256) NOT NULL);
+-- Create a databse hbtn_0d_usa and a table named cities
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL);
