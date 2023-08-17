9852733b0afb49f11521fbcdff04eb872fdf0556
Euwamahoro <e.uwamahoro@alustudent.com>
2023-08-17 08:27:43 -0700

    This is Enock

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -24,18 +24,18 @@ class TestBase_instantiation(unittest.TestCase):
     def test_no_arg(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+        self.assertEqual(b1.id, b2.id + 1)
 
     def test_three_bases(self):
         b1 = Base()
         b2 = Base()
         b3 = Base()
-        self.assertEqual(b1.id, b3.id - 2)
+        self.assertEqual(b1.id, b3.id + 2)
 
     def test_None_id(self):
         b1 = Base(None)
         b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+        self.assertEqual(b1.id, b2.id + 1)
 
     def test_unique_id(self):
         self.assertEqual(12, Base(12).id)
@@ -44,7 +44,7 @@ class TestBase_instantiation(unittest.TestCase):
         b1 = Base()
         b2 = Base(12)
         b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
+        self.assertEqual(b1.id, b3.id + 1)
 
     def test_id_public(self):
         b = Base(12)
