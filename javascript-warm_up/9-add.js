0e3cc17c4a96388b0e935c74d03db9fb50c3dc3a
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-27 15:25:10 -0700

    This is Enock

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
 function add(a, b) {
-	console.log(a + b);
+  return a + b;
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!isNaN(arg1) && !isNaN(arg2)) {
+  console.log(add(arg1, arg2));
+} else {
+  console.log('NaN');
 }
