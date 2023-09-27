b86bf1ffa6892f5f2de2de8202016f25d4a1f768
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-27 12:27:59 -0700

    This is Enock

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,5 +3,8 @@
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-  console.log('undefined is undefined');
+  const arg1 = process.argv[2] !== undefined ? process.argv[2] : 'HBTN';
+  const arg2 = process.argv[3] !== undefined ? process.argv[3] : 'HBTN';
+  console.log(arg1 + ' is ' + arg2);
 }
+
