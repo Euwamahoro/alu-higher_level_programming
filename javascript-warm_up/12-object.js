36085518cd3d6d27c17c031f6bb8b7c481fae09b
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-27 13:29:30 -0700

    This is Enock

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 
 const myObject = [[{type: 'object', value: 12}], [{type: 'object', value: 89}]];
-  console.log(myObject);
+for (const nestedArray of myObject) {
+    for (const obj of nestedArray) {
+        console.log(obj);
+    }   
+}
