f337f8d921837fa83731af529f4a72ad5e456607
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-27 13:03:03 -0700

    This is Enock

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const firstArg = process.argv[2];
+const firstArg = process.argv[2]; // Get the first argument
+
 if (/^-?\d+$/.test(firstArg)) {
-    const intValue = parseInt(firstArg);
-    console.log(`My number: ${intValue}`);
+  const intValue = parseInt(firstArg); // Convert to an integer
+  console.log(`My number: ${intValue}`);
 } else {
-    console.log('Not a number');
+  console.log("Not a number");
 }
