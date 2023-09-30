136b126f207ef3ccf97f2bfc4600eea776f01ec8
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-30 08:37:04 -0700

    This is Enock

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -3,9 +3,15 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
     this.size = size;
+  }
+
+  print() {
+    for (let i = 0; i < this.size; i++) {
+      console.log('X'.repeat(this.size));
+    }
   }
 }
 
