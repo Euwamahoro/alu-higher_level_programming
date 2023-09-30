e33894ea854cd5627ba9a23d50e9c063469fe478
Euwamahoro <e.uwamahoro@alustudent.com>
2023-09-30 06:39:55 -0700

    This is Enock

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,12 +4,11 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
       // Create an empty object
-      return {};
-    } else {
+      return;
+    }
       // Initialize width and height attributes
       this.width = w;
       this.height = h;
-    }
   }
 }
 
